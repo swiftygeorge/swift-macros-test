f499f2d22c47c4c789a6868bfbcf38b5026f6159
swiftygeorge <119117609+swiftygeorge@users.noreply.github.com>
2023-06-05 21:34:21 -0400

    Testing Swift Previews in UIKit

--- SwiftMacrosTest/ViewController.swift
+++ SwiftMacrosTest/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
+    
+    fileprivate let label: UILabel = {
+        let label = UILabel()
+        label.text = "Me loves Swift Macros 😀"
+        label.font = .preferredFont(forTextStyle: .body)
+        label.textColor = .label
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        view.addSubview(label)
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        label.widthAnchor.constraint(lessThanOrEqualTo: view.layoutMarginsGuide.widthAnchor).isActive = true
     }
 
 
+}
+
+/*
+struct ViewControllerRepresentable: UIViewControllerRepresentable {
+    typealias UIViewControllerType = ViewController
+    
+    func makeUIViewController(context: Context) -> ViewController {
+        return ViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+        
+    }
+}
+ */
+
+#Preview(traits: .landscapeLeft) {
+    ViewController()
 }
 
